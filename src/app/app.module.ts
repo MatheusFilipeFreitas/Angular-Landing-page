d1bf55d8d908c692028d1be8d13c895ffe70348d
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2022-05-17 03:04:12 -0300

    commit#1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavComponent } from './landing-page/nav/nav.component';
+import { ContentComponent } from './landing-page/content/content.component';
+import { FooterComponent } from './landing-page/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
